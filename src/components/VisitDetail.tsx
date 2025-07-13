432b344976aa32303dc46ab121b3a9615b1ac49b
jacobwadkins <jacob.wadkins@outlook.com>
2025-07-13 10:32:37 -0400

    Fix useRef import error

--- src/components/VisitDetail.tsx
+++ src/components/VisitDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { forwardRef, useImperativeHandle } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
